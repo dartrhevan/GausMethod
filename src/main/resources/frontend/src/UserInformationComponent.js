9d7ab8793530da1ff28e7e94f3b20d857ffeb296
dartrhevan <vovasatunkin@mail.ru>
2019-12-27 12:09:26 +0500

    editing user data is not working

--- src/main/resources/frontend/src/UserInformationComponent.js
+++ src/main/resources/frontend/src/UserInformationComponent.js
@@ -6,7 +6,10 @@ export default class UserInformationComponent extends React.Component {
         super();
         this.state = {user: {   }};
         this.initUser = this.initUser.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
     }
+
+
     onSubmit(event) {
         if($('#password').val() !== $('#passwordConf').val()) {
             event.preventDefault();
@@ -15,6 +18,18 @@ export default class UserInformationComponent extends React.Component {
     }
 
     async componentDidMount() {
+        const check = ((i, e) =>
+        {
+            if(!this.state.user.hasOwnProperty(e.name))
+                return false;
+            return e.value !== this.state.oldUser[e.name];
+        }).bind(this);
+        $("input").on('input', event => {
+            if(!this.state.user.hasOwnProperty(event.target.name))
+                this.state.user[event.target.name] = event.target.value;
+            let a = $('input').is(check);
+            $("#submit").disabled = !a;
+        });
         await $.get('/api/get_user_data', this.initUser);
     }
 
@@ -25,7 +40,7 @@ export default class UserInformationComponent extends React.Component {
             console.log(obj.error);
         else
         {
-            this.setState({user: obj});
+            this.setState({user: obj, oldUser: obj});
         }
     }
 
@@ -35,7 +50,7 @@ export default class UserInformationComponent extends React.Component {
                 <h2>Information about you</h2>
                 E-mail
                 <br/>
-                <input type='email' value={this.state.user.email} placeholder='email' required className='inputRow' name='email'/>
+                <input type='email' value={this.state.user.email} readOnly='false' placeholder='email' required className='inputRow' name='email'/>
                 NickName
                 <br/>
                 <input type='text' value={this.state.user.nickname} placeholder='nickname' required className='inputRow' name='nickname'/>
@@ -57,7 +72,7 @@ export default class UserInformationComponent extends React.Component {
                 Old password
                 <br/>
                 <input type='password' placeholder='old password' name="password" required className='inputRow'/>
-                <button type='submit'>Submit</button>
+                <button type='submit' disabled="disabled" id='submit'>Submit</button>
             </form>
         );
     }

70d770e1e90169f965fd11bbf8d3f1edd23a8f3c
dartrhevan <vovasatunkin@mail.ru>
2020-01-04 18:49:11 +0500

    comments section simple version

--- src/main/resources/frontend/src/CommentsComponent/CommentComponent.js
+++ src/main/resources/frontend/src/CommentsComponent/CommentComponent.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './comments.css';
 
 export default class CommentComponent extends React.Component{
     render() {
         return (
         <div className='comment'>
-            {this.props.comment}
+            <div className='commentInf'>
+                <div className='nick'>{this.props.nick}</div>
+                <div className='activ'>{this.props.activity}</div>
+                <div className='date'>{this.props.date}</div>
+            </div>
+            <div className='commentText'>
+                {this.props.comment}
+            </div>
         </div>);
     }
 }
--- src/main/resources/frontend/src/CommentsComponent/CommentsComponent.js
+++ src/main/resources/frontend/src/CommentsComponent/CommentsComponent.js
@@ -6,8 +6,17 @@ export default class CommentsComponent extends React.Component {
     constructor() {
         super();
         this.state = {comments: []};
-        this.componentDidMount = this.componentDidMount.bind(this);
+        //this.componentDidMount = this.componentDidMount.bind(this);
+
+        this.ws = new WebSocket(`ws://${window.location.host}/comments`);
+        this.ws.onopen = ev => {this.ws.send(this.props.title)};
+        const onMes = mes => {
+            console.log(mes.data);
+            this.setState({comments: JSON.parse(mes.data)});
+        };
+        this.ws.onmessage = onMes.bind(this);
     }
+    /*
     componentDidMount() {
         console.log(this.props.title);
         this.ws = new WebSocket(`ws://${window.location.host}/comments`);
@@ -18,12 +27,12 @@ export default class CommentsComponent extends React.Component {
         };
         this.ws.onmessage = onMes.bind(this);
     }
-
+*/
     render() {
         return (
-        <form method='post' action='/api/add_comment' className='commentSection content'>
+        <form method='post' action='/api/add_comment' id='commentSection' className='content'>
             <p>Comments</p>
-            {this.state.comments.map(c => <CommentComponent comment={c.comment}/>)}
+            {this.state.comments.map(c => <CommentComponent comment={c.comment} date={c.date} nick={c.author.nickname} activity={c.author.activity}/>)}
             <textarea name='comment' className='inputRow'></textarea>
             <br/>
             <input type='hidden' name='origin' value='Systems'/>

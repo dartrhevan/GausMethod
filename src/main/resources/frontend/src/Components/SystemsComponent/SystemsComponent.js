a5f92e539fbcdc42ee43fee686d476689da65648
dartrhevan <vovasatunkin@mail.ru>
2020-01-09 18:17:31 +0500

    systems description has been added

--- src/main/resources/frontend/src/Components/SystemsComponent/SystemsComponent.js
+++ src/main/resources/frontend/src/Components/SystemsComponent/SystemsComponent.js
@@ -36,7 +36,7 @@ class SystemsComponent extends CalculatorComponent {
           this.setState(this.getNewState({result: s}));
     };
     this.state = {
-        types: [new Type('Gaus', this.getDescription(), gausHdl.bind(this)), new Type('Run', 'awfdgsfdg', runHdl.bind(this))],
+        types: [new Type('Gaus', SystemsComponent.getGaussDescription(), gausHdl.bind(this)), new Type('Tridiagonal matrix', SystemsComponent.getTridiagonalDescription(), runHdl.bind(this))],
         dimension: 2, current: 0, result: null, title: "Systems"
     };
     this.getCalculator = this.getCalculator.bind(this);
@@ -68,39 +68,46 @@ class SystemsComponent extends CalculatorComponent {
       </>);
   }
 
-  getDescription() {
-      return (<div className='description'>
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
+  static getGaussDescription() {
+      return (<>
+          Gaussian elimination, also known as row reduction, is an algorithm in linear algebra for solving a system of linear equations. It is usually understood as a sequence of operations performed on the corresponding matrix of coefficients. This method can also be used to find the rank of a matrix, to calculate the determinant of a matrix, and to calculate the inverse of an invertible square matrix. The method is named after Carl Friedrich Gauss (1777–1855).
           <br/>
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
+          To perform row reduction on a matrix, one uses a sequence of elementary row operations to modify the matrix until the lower left-hand corner of the matrix is filled with zeros, as much as possible. There are three types of elementary row operations:
+          <ul>
+              <li>Swapping two rows,</li>
+              <li>Multiplying a row by a nonzero number,</li>
+              <li>Adding a multiple of one row to another row.</li>
+          </ul>
+          Using these operations, a matrix can always be transformed into an upper triangular matrix, and in fact one that is in row echelon form. Once all of the leading coefficients (the leftmost nonzero entry in each row) are 1, and every column containing a leading coefficient has zeros elsewhere, the matrix is said to be in reduced row echelon form. This final form is unique; in other words, it is independent of the sequence of row operations used. For example, in the following sequence of row operations (where multiple elementary operations might be done at each step), the third and fourth matrices are the ones in row echelon form, and the final matrix is the unique reduced row echelon form.
           <br/>
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
+          <img src='/img/gaussExample1.png'/>
+          <img src='/img/gaussExample2.png'/>
           <br/>
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
+      </>)
+  }
+
+  static getTridiagonalDescription() {
+      return (<>
+          In numerical linear algebra, the tridiagonal matrix algorithm, also known as the Thomas algorithm (named after Llewellyn Thomas), is a simplified form of Gaussian elimination that can be used to solve tridiagonal systems of equations. A tridiagonal system for n unknowns may be written as
           <br/>
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
+          <img src='/img/triadiagonalExample1.png'/>
+          <br/>where a1=0, and cn=0.
           <br/>
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
+          <img src='/img/tridiagonalExample2.png'/>
           <br/>
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
-          hghjjbskjafsfaldm
+          The forward sweep consists of modifying the coefficients as follows, denoting the new coefficients with primes:
           <br/>
-      </div>)
+          <img src='/img/cs.png'/>
+          <br/>
+          and
+          <br/>
+          <img src='/img/ds.png'/>
+          <br/>
+          The solution is then obtained by back substitution:
+          <br/>
+          <img src='/img/sln.png'/>
+          <br/>
+      </>)
   }
 /*
   getTypes() {

c7996492c929c6b5d91ae99a9e007460c3056a0c
dartrhevan <vovasatunkin@mail.ru>
2020-02-29 20:57:53 +0500

    lost files added

---
+++ src/main/resources/frontend/build/precache-manifest.2b8b4d237caeac505c5c34e519d01b59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811b24c27dc9aafcd94b4df266bed352",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84e50d6c4bc5c774643d",
+    "url": "/static/css/main.a0a55bdc.chunk.css"
+  },
+  {
+    "revision": "d16bb65e2aae0268d998",
+    "url": "/static/js/2.dfdf0ebc.chunk.js"
+  },
+  {
+    "revision": "84e50d6c4bc5c774643d",
+    "url": "/static/js/main.1f1a7ea0.chunk.js"
+  },
+  {
+    "revision": "75d4525b14332a664fee",
+    "url": "/static/js/runtime-main.0da8c5e8.js"
+  }
+]);
